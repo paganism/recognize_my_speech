09c160981e085aba01d90cabec753eb3495b41c3
Yuriy Belov <paganism@mail.ru>
2021-09-29 21:45:24 +0300

    fixed run tg bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,8 +1,7 @@
 from environs import Env
 
-from telegram import Update
 from telegram.ext import Updater, CommandHandler, \
-    MessageHandler, Filters, CallbackContext
+    MessageHandler, Filters
 
 from dialogflow_respond_peocess import detect_intent_texts
 import logging
@@ -13,31 +12,43 @@ import google.api_core.exceptions
 
 log = logging.getLogger(__file__)
 
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
+
+def start(update, context):
     user = update.effective_user
     update.message.reply_text(
         f'Здравствуйте, {user["username"]}'
     )
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
+def help_command(update, context):
     update.message.reply_text('Этот бот отвечает на ваши вопросы')
 
 
-def bot_respond(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
+def bot_respond(update, context):
     try:
         respond = detect_intent_texts(
-            project_id, update.message.chat.id, update.message.text, language_code
+            project_id,
+            update.message.chat.id,
+            update.message.text,
+            language_code
         )
+        if respond:
+            update.message.reply_text(respond.fulfillment_text)
     except google.api_core.exceptions.ServiceUnavailable:
         log.debug("DialogFlow недоступен")
 
-    except Exception as ex:
-        log.debug(ex)
-    update.message.reply_text(respond.fulfillment_text)
+
+def run_bot(token):
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(
+        MessageHandler(Filters.text & ~Filters.command, bot_respond)
+    )
+
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
@@ -46,28 +57,12 @@ if __name__ == '__main__':
     env.read_env()
 
     token = env('TELEGRAM_TOKEN')
-    session_id = env('CHAT_ID')
     project_id = env('PROJECT_ID')
     language_code = env('LANGUAGE_CODE')
 
-    updater = Updater(token)
-    
-    dispatcher = updater.dispatcher
-
-    handler = TGBotLogHandler(session_id, token)
+    handler = TGBotLogHandler(env('CHAT_ID'), token)
     handler.setLevel(logging.DEBUG)
     log.addHandler(handler)
+    log.info('Бот TG запущен')
 
-    log.info('Бот запущен')
-
-    dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-
-    dispatcher.add_handler(
-        MessageHandler(Filters.text & ~Filters.command, bot_respond)
-    )
-
-    updater.start_polling()
-    updater.idle()
+    run_bot(token)
